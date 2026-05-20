f6af4ead1bf895137cfc6f747744661dada51335
Ganerdene Erdenebayar <gerdeneb@htl-steyr.ac.at>
2026-05-20 13:43:56 +0200

    edited first implementation of game view

--- src/main/java/htl/steyr/tetris/GameController.java
+++ src/main/java/htl/steyr/tetris/GameController.java
@@ -1,4 +1,12 @@
 package htl.steyr.tetris;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
 public class GameController {
+    public Pane gamePane;
+    public Pane nextShapePane;
+    public Label scoreLabel;
+
+
 }
